0418cec46c0989929a200645b51536a93397c1b0
jinhongkai <1258245377@qq.com>
2018-11-29 09:33:12 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:jhk
+ * createTime:2018-11-29 09:08
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交1111111111");
+    }
+}
